df132b0d3eb49b79faecf20d77691193fe7c062e
Fatih Kabak <fatihkabak39@hotmail.com>
2021-02-18 00:11:07 +0300

    17 Şubat

--- Business/Concrete/ProductManager.cs
+++ Business/Concrete/ProductManager.cs
@@ -1,6 +1,7 @@
 ﻿using Business.Abstract;
 using Business.Constants;
 using Business.ValidationRules.FluentValidation;
+using Core.Aspects.Autofac.Validation;
 using Core.CrossCuttingConcerns.Validation;
 using Core.Utilities.Results;
 using DataAccess.Abstract;
@@ -22,10 +23,9 @@ namespace Business.Concrete
             _productDal = productDal;
         }
 
+        [ValidationAspect(typeof(ProductValidator))]
         public IResult Add(Product product)
         {
-
-            ValidationTool.Validate(new ProductValidator(), product);
 
             //Business Codes
 
